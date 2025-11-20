daf8c548dbedfc2d455206b23d1941e3d4dd6e59
Nguyende289 <ngde.bg@gmail.com>
2025-11-21 00:57:18 +0700

    Add TrafficAccidents component with form and list

---
+++ src/pages/TrafficAccidents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AccidentForm from "../components/AccidentForm";
+import AccidentList from "../components/AccidentList";
+
+export default function TrafficAccidents() {
+  return (
+    <div>
+      <AccidentForm />
+      <AccidentList />
+    </div>
+  );
+}
